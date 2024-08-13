a1574b669a965bc55acffef2952b4f200d6781ea
mccampero <mccampero2@gmail.com>
2024-08-13 11:47:49 -0400

    add cats and home page and more styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat-index'),
     # Routes will be added here
 ]
