b3bee141650d40ee906ff0ba7211bb329ffad3e7
mccampero <mccampero2@gmail.com>
2024-08-14 11:03:44 -0400

    add cat model and detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+from .models import Cat
 
 
 
@@ -8,27 +8,17 @@ from django.http import HttpResponse
 # Create your views here.
 def about(request):
     return render(request, 'about.html')
-# views.py
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-# views.py
 
 def cat_index(request):
     # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
 def home(request):
     return render(request, 'home.html')
+
+# views.py
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
